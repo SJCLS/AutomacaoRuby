532d3133e41285ca5d639c753da4bbb2bd0d7296
Samuel Jonathan <samueljonathanleumas@gmail.com>
2024-10-31 12:57:47 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,29 +1,18 @@
-require'capybara'
-require'capybara/dsl'
-require'capybara/rspec/matchers'
-require'selenium-webdriver'
-require'rspec'
+require 'capybara'
+require 'capybara/dsl'
+require 'capybara/rspec/matchers'
+require 'selenium-webdriver'
+require 'rspec'
 
 World(Capybara::DSL)
 World(Capybara::RSpecMatchers)
 
-Capybara.register_driver :selenium do |app|
-    Capybara::Selenium::Driver.new(
-        app,
-        browser: :chrome,
-        desired_capabilities:Selenium::WebDriver::Remote::Capabilities.chrome(
-            'chromeOptions'=> {'args' => ['--disable-infobar','window-size=1600,1024']}
-        )
-    )
-end 
-
-Capybara.configure do |config|
-    config.default_driver = :selenium
-    config.default_max_wait_time = 10
-    config.app_host = 'https://cursos.alura.com.br/loginForm?urlAfterLogin=https://cursos.alura.com.br/dashboard'
+Capybara.register_driver :selenium_chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: Selenium::WebDriver::Chrome::Options.new(args: ['--disable-infobars', 'window-size=1600,1024']))
 end
 
-
-
-
-
+Capybara.configure do |config|
+  config.default_driver = :selenium_chrome
+  config.default_max_wait_time = 10
+  config.app_host = 'https://cursos.alura.com.br'
+end
